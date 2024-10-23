7c1787cafb66098eb69539f327c8952232bb752d
Kresley Lucas <96209978+klucas27@users.noreply.github.com>
2024-10-23 17:46:47 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+// // Rolagem suave para âncoras no menu
+// document.querySelectorAll('nav ul li a').forEach(anchor => {
+//     anchor.addEventListener('click', function(e) {
+//         e.preventDefault(); // Previne o comportamento padrão do link
+
+//         const targetId = this.getAttribute('href');
+//         const targetElement = document.querySelector(targetId);
+        
+//         // Rolagem suave
+//         targetElement.scrollIntoView({
+//             behavior: 'smooth'
+//         });
+//     });
+// });
